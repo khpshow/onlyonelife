d839f402a1b3770363940143bdbd0751b6b6b593
khpshow <159820774+khpshow@users.noreply.github.com>
2024-02-13 19:59:21 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -100,7 +100,7 @@ const IndexPage = () => {
         bgColor={'var(--standard-light-grey)'}
         title={'about Sydney'}
         quote={
-          '“We believe in two things: the pursuit of quality in everything we do, and looking after one another. Everything else should take care of itself.”'
+          '“우리는 We believe in two things: the pursuit of quality in everything we do, and looking after one another. Everything else should take care of itself.”'
         }
       />
 
