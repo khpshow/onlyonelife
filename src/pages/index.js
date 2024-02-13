c70d79a610634aec1ea737b19dd50dccbae65a41
khpshow <159820774+khpshow@users.noreply.github.com>
2024-02-13 19:58:14 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -79,7 +79,7 @@ const IndexPage = () => {
             miniImage={'/highlightmin.png'}
             miniImageAlt={'mini highlight image'}
             title={'Luxury Knitwear'}
-            description={`This soft lambswool jumper is knitted in Scotland, using yarn from one of the world's oldest spinners based in Fife`}
+            description={`이것은 This soft lambswool jumper is knitted in Scotland, using yarn from one of the world's oldest spinners based in Fife`}
             textLink={'shop now'}
             link={'/shop'}
           />
